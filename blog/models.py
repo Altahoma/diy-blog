40324278688ccc0f8c13f0ce2639dab7e7036552
altahoma <altahoma@gmail.com>
2022-04-24 00:08:32 +0300

    Created models for Author, Blog, Comment

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.urls import reverse
+import datetime
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    bio = models.TextField(max_length=1000)
+    reg_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    class Meta:
+        ordering = ['-reg_date']
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=5000)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog', args=[str(self.id)])
+
+    class Meta:
+        ordering = ['-pub_date']
+
+
+class Comment(models.Model):
+    description = models.TextField(max_length=500)
+    author = models.ForeignKey('Author', on_delete=models.SET('Author DELETED'))
+    blog = models.ForeignKey('Blog', on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.description[:75]
+
+    class Meta:
+        ordering = ['pub_date']
