7050682bd1d54f0a8dbf5438947cd844c5775b91
altahoma <altahoma@gmail.com>
2022-04-26 20:13:04 +0300

    Created home page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Author, Blog, Comment
 
-# Create your views here.
+
+def index(request):
+    num_authors = Author.objects.all().count()
+    num_blogs = Blog.objects.all().count()
+    num_comments = Comment.objects.all().count()
+    context = {
+        'num_authors': num_authors,
+        'num_blogs': num_blogs,
+        'num_comments': num_comments,
+    }
+
+    return render(request, 'index.html', context=context)
